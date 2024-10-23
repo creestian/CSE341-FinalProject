cc944d1ce6728f785e05eb916270969ac9e14082
creestian <creestian@gmail.com>
2024-10-23 00:20:43 -0300

    update swagger

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'HTTP Documentation',
         description: 'CSE 341 Final project\n Danny Ellis, Angie Pincock, Cristian Fabian Navia, Berny Fred',
     },
-    host:'cse341-finalproject-60ke.onrender.com',
+    host:'creestian.onrender.com',
     scheme:['https','http']
 };
 

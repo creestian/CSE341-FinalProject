fd944232688d48ffbe499930648f6fb54ce8a3c2
angievab <angiepincock@gmail.com>
2024-10-28 01:35:46 -0500

    authentication + testing

--- routes/authors.js
+++ routes/authors.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = require('express').Router();
-
 const authorsController = require('../controllers/authors');
+//const validation = require('../middleware/validate');
+const {isAuthenticated} = require('../middleware/authenticate');
 
-// Add isAuthenticated to all routes after testing
-// Add validation to all routes.
-// Needs router.get('/:first, last name or both?)
+
+
 router.get('/', authorsController.getAll);
 
 router.get('/:id', authorsController.getSingle);
 
-router.post('/',  authorsController.createAuthor);
+router.post('/', isAuthenticated, authorsController.createAuthor);
 
-router.put('/:id',  authorsController.updateAuthor);
+router.put('/:id', isAuthenticated, authorsController.updateAuthor);
 
-router.delete('/:id', authorsController.deleteAuthor);
+router.delete('/:id', isAuthenticated, authorsController.deleteAuthor);
 
 module.exports = router;
